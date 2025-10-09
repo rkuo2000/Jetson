62130b07edc352d696493fc5ee49ae712abdf617
Richard Kuo <rkuo2000@gmail.com>
2025-10-10 02:57:44 +0800

    Delete yolo11_export.py

--- yolo11_export.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# Load a YOLO11n PyTorch model
-model = YOLO("yolo11n.pt")
-
-# Export the model to TensorRT
-model.export(format="engine") # FP32
-#model.export(format="engine",int8=True) # INT8
