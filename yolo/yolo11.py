82a6d592c835ad2cafe8a8c966683fcc8809d982
Richard Kuo <rkuo2000@gmail.com>
2025-11-07 01:18:09 +0800

    Update yolo11.py

--- yolo/yolo11.py
+++ yolo/yolo11.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo11n.pt")
+model = YOLO("yolo11s.pt")
 
 #results = model("https://ultralytics.com/images/bus.jpg")
-results = model("bus.jpg")
+results = model("images/bus.jpg")
 
 results[0].show()
